7b777d6227117b65fbfc74292fe5e6ec3e51c483
lihongzhang <617391485@qq.com>
2024-08-27 00:22:03 +0800

    提交

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    这是首页
+  </div>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <div class="login-body">
+
+    </div>
+  </div>
+</template>
+
+<style>
+.login {
+  width: 100vw;
+  height: 100vh;
+  background: url(../image/login/bg.jpg);
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-body{
+  width: 500px;
+  height: 300px;
+  background: #fff;
+  border-radius: 12px;
+}
+</style>
