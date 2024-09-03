bd3cdacb0dcc64eb92aa060ea184815c5aea6592
lihongzhang <617391485@qq.com>
2024-09-03 23:57:49 +0800

    用户登录完善，增加更改密码和token逻辑

---
+++ src/components/updatePwd.vue
@@ -0,0 +1,98 @@
+<template>
+    <el-dialog class="u-modal" v-model="isShowModal" title="更改密码" width="450" draggable overflow @close="handleCloseModal">
+        <el-form class="update-pwd-form" ref="userForm" :model="form" :rules="rules" label-width="100px">
+            <el-form-item label="旧密码：" prop="oldPwd">
+                <el-input v-model="form.oldPwd" placeholder="请输入旧密码" />
+            </el-form-item>
+            <el-form-item label="新密码：" prop="newPwd">
+                <el-input v-model="form.newPwd" placeholder="请输入新密码" />
+            </el-form-item>
+            <el-form-item label="确认密码：" prop="surePwd">
+                <el-input v-model="form.surePwd" placeholder="请输入确认密码：" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="handleCloseModal">取消</el-button>
+                <el-button type="primary" @click="saveUserHandle">确认</el-button>
+            </div>
+        </template>
+    </el-dialog>
+</template>
+<script setup>
+import { ref, defineEmits, watch } from 'vue'
+import axios from 'axios'
+
+const props = defineProps(['visible', 'userName'])
+const emit = defineEmits(['closeModal', 'updateData'])
+
+const isShowModal = ref(false)
+const userForm = ref(null)
+const form = ref({
+    oldPwd: '',
+    newPwd: '',
+    surePwd: ''
+})
+
+watch(
+    () => props.visible,
+    (val) => {
+        isShowModal.value = val
+    },
+    { immediate: true }
+)
+
+const validatePasswordMatch = (rule, value, callback) => {
+    if (value !== form.value.newPwd) {
+        callback(new Error('两次输入的密码不一致'));
+    } else {
+        callback();
+    }
+};
+
+const rules = ref({
+    oldPwd: [{ required: true, message: '旧密码不能为空', trigger: 'change' }],
+    newPwd: [{ required: true, message: '新密码不能为空', trigger: 'change' }],
+    surePwd: [
+        { required: true, message: '确认密码不能为空', trigger: 'change' },
+        { validator: validatePasswordMatch, trigger: 'change' }
+    ]
+})
+
+const saveUserHandle = () => {
+    userForm.value.validate((valid) => {
+        if (valid) {
+            let params = {
+                userName: props.userName,
+                oldPwd: form.value.oldPwd,
+                newPwd: form.value.newPwd
+            }
+            axios.post('/api/users/updatePassword', params).then(res => {
+                if (res.data.code === '0') {
+                    ElMessage({
+                        message: '密码更新成功！',
+                        type: 'success',
+                    })
+                    handleCloseModal()
+                } else {
+                    ElMessage.error('密码更新失败！')
+                }
+            })
+        }
+    })
+}
+
+const handleCloseModal = () => {
+    // 清空表单
+    form.value.oldPwd = '';
+    form.value.newPwd = '';
+    form.value.surePwd = '';
+    userForm.value.resetFields();
+    emit('closeModal', '子组件通知父组件了')
+}
+</script>
+<style scoped>
+.update-pwd-form {
+    padding: 0 30px 0 20px;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,34 +2,36 @@
   <div class="login">
     <div class="login-body">
       <img class="login-img" src="../image/login/login-img.png" alt="login image" />
-      <el-form
-        ref="loginForm"
-        class="login-form"
-        :model="form"
-        :rules="rules"
-        label-width="auto"
-        style="max-width: 300px"
-      >
+      <el-form ref="loginForm" class="login-form" :model="form" :rules="rules" label-width="auto"
+        style="max-width: 300px">
         <el-form-item prop="name">
           <el-input v-model="form.name" placeholder="请输入用户名" />
         </el-form-item>
         <el-form-item prop="pwd">
           <el-input v-model="form.pwd" type="password" placeholder="请输入密码" />
         </el-form-item>
-        <el-form-item>
+        <el-form-item class="login-btn-wrap">
           <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
         </el-form-item>
+        <div class="update-pwd">
+          <el-button link type="primary" size="small" @click="updatePwdHandle">更改密码</el-button>
+        </div>
       </el-form>
     </div>
+    <updatePwd v-if="showModal" :visible.sync="showModal" :userName.sync="form.name" @closeModal="closeModalHandle">
+    </updatePwd>
   </div>
 </template>
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
+import updatePwd from '../components/updatePwd.vue'
+import axios from 'axios'
 
 const router = useRouter()
 const loginForm = ref(null)
+const showModal = ref(false);
 
 const form = ref({
   name: '',
@@ -41,12 +43,39 @@ const rules = {
   pwd: [{ required: true, message: '密码不能为空', trigger: 'blur' }]
 }
 
+const updatePwdHandle = () => {
+  showModal.value = true;
+}
+
+const closeModalHandle = () => {
+  showModal.value = false;
+}
+
 const onSubmit = () => {
-  loginForm.value.validate((valid) => {
+  loginForm.value.validate(valid => {
     if (valid) {
-      router.push({ name: 'home' })
-    } else {
-      // ElMessage.error('请正确填写表单');
+      let param = {
+        userName: form.value.name,
+        password: form.value.pwd
+      }
+      console.log(param, 'param')
+      try {
+        axios.post('/api/users/login', param).then(res => {
+          console.log(res, 'resres')
+          if (res.data.token) {
+            localStorage.setItem('token', res.data.token);
+            ElMessage.success('登录成功')
+            router.push({ name: 'home' })
+          } else {
+            ElMessage.error(res.data.message || '登录失败，请重试');
+          }
+        }).catch(err => {
+          ElMessage.error(err.response.data.message || '登录失败，请重试');
+        })
+      } catch (error) {
+        ElMessage.error('登录失败，请检查用户名或密码');
+        console.error('登录失败：', error);
+      }
     }
   })
 }
@@ -61,6 +90,7 @@ const onSubmit = () => {
   justify-content: center;
   align-items: center;
 }
+
 .login-body {
   width: 650px;
   height: 300px;
@@ -69,17 +99,29 @@ const onSubmit = () => {
   padding: 20px;
   display: flex;
 }
+
 .login-img {
   width: 400px;
   height: 300px;
 }
+
 .login-form {
   padding-left: 40px;
   display: flex;
   flex-direction: column;
   justify-content: center;
 }
+
 .login-btn {
   width: 100%;
+}
+
+.login-btn-wrap {
+  margin-bottom: 10px;
+}
+
+.update-pwd {
+  display: flex;
+  justify-content: center;
 }
 </style>
